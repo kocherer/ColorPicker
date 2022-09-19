3b1ee4b0913174ded6976da263f904b23241d962
kocherer <84465592+kocherer@users.noreply.github.com>
2022-09-19 17:28:41 -0500

    Update colorPickerFunction.js

--- colorPickerFunction.js
+++ colorPickerFunction.js
@@ -7,17 +7,17 @@ red.addEventListener("keyup", function (event) {
    r = red.value;
    if (!r)
       r = 0;
-   box.style.backgroundColor = `rgb((${r}/255), (${g}/255), (${b}/255))`;
+   box.style.backgroundColor = `rgb(${r}/255, ${g}/255, ${b}/255)`;
 });
 green.addEventListener("keyup", function (event) {
    g = green.value;
    if (!g)
       g = 0;
-   box.style.backgroundColor = `rgb((${r}/255), (${g}/255), (${b}/255))`;
+   box.style.backgroundColor = `rgb(${r}/255, ${g}/255, ${b}/255)`;
 });
 blue.addEventListener("keyup", function (event) {
    b = blue.value;
    if (!b)
       b = 0;
-   box.style.backgroundColor = `rgb((${r}/255), (${g}/255), (${b}/255))`;
+   box.style.backgroundColor = `rgb(${r}/255, ${g}/255, ${b}/255)`;
 });
