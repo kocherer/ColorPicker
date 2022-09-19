b7062a73b31e43d2945384706073f3b4a0c8cd83
kocherer <84465592+kocherer@users.noreply.github.com>
2022-09-19 16:48:17 -0500

    Update colorPickerFunction.js

--- colorPickerFunction.js
+++ colorPickerFunction.js
@@ -8,23 +8,23 @@ red.addEventListener("keyup", function (event) {
    r = red.value;
    if (!r)
       r = 0;
-   box.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${l}) / 255`;
+   box.style.backgroundColor = `rgba(${r}/255, ${g}/255, ${b}/255, ${l})`;
 });
 green.addEventListener("keyup", function (event) {
    g = green.value;
    if (!g)
       g = 0;
-   box.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${l}) / 255`;
+   box.style.backgroundColor = `rgba(${r}/255, ${g}/255, ${b}/255, ${l})`;
 });
 blue.addEventListener("keyup", function (event) {
    b = blue.value;
    if (!b)
       b = 0;
-   box.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${l}) / 255`;
+   box.style.backgroundColor = `rgba(${r}/255, ${g}/255, ${b}/255, ${l})`;
 });
 light.addEventListener("keyup", function (event) {
    l = light.value;
    if (!l)
       l = 0;
-   box.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${l}) / 255`;
+   box.style.backgroundColor = `rgba(${r}/255, ${g}/255, ${b}/255, ${l})`;
 });
