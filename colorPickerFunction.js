7ec301eab10c97ff100ffa31b606541f1dd89862
kocherer <84465592+kocherer@users.noreply.github.com>
2022-09-19 21:38:56 -0500

    Update colorPickerFunction.js

--- colorPickerFunction.js
+++ colorPickerFunction.js
@@ -1,23 +1,25 @@
-let red = document.getElementById('red');
-let green = document.getElementById('green');
-let blue = document.getElementById('blue');
-let box = document.querySelector('div.valueBox');
-let r = 0, g = 0, b = 0;
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+const box = document.querySelector('div.valueBox');
+const r = 0;
+const g = 0;
+const b = 0;
 red.addEventListener("keyup", function (event) {
    r = red.value;
    if (!r)
       r = 0;
-   box.style.backgroundColor = `rgb(${r}, ${g}, ${b})` + `/255`;
+   box.style.backgroundColor = `rgb(${r}/255, ${g}/255, ${b}/255)`;
 });
 green.addEventListener("keyup", function (event) {
    g = green.value;
    if (!g)
       g = 0;
-   box.style.backgroundColor = `rgb(${r}, ${g}, ${b})` + `/255`;
+   box.style.backgroundColor = `rgb(${r}/255, ${g}/255, ${b}/255)`;
 });
 blue.addEventListener("keyup", function (event) {
    b = blue.value;
    if (!b)
       b = 0;
-   box.style.backgroundColor = `rgb(${r}, ${g}, ${b})` + `/255`;
+   box.style.backgroundColor = `rgb(${r}/255, ${g}/255, ${b}/255)`;
 });
